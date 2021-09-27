848c081a1146ca46d939646f3c044d650f6d93da
Tvaki <90245933+Tvaki@users.noreply.github.com>
2021-09-27 19:22:19 +0300

    Add files via upload

---
+++ Лабораторна робота 1 ОП/Laboratorna rabota 1 C++/Laboratorna rabota 1 C++/Laboratorna rabota 1 C++.cpp
@@ -0,0 +1,19 @@
+﻿// Laboratorna rabota 1 C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+	//14 Перевести задане значення кута із радіанної міри в градусну
+	double RadB, GradB;
+	printf("Enter radians ");
+	std::cin >> RadB;
+	GradB = RadB * 180 / 3.14159265;
+	printf("Get degrees ");
+	std::cout << GradB;
+	return 0;
+}
+
+
---
+++ Лабораторна робота 1 ОП/Laboratorna rabota Python 1/Laboratorna rabota Python 1/Laboratorna_rabota_Python_1.py
@@ -0,0 +1,5 @@
+import math
+#14 Перевести задане значення кута із радіанної міри в градусну
+RadB=int(input('Enter radians '))
+GradB=(RadB*(180/math.pi))
+print('Get degrees ',GradB)
